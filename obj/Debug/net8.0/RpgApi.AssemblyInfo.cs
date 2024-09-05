1a973e188cb364ed7e8bcfc793e894e18b948df8
Danillo de Lacerda <danillo.lacerda77@gmail.com>
2024-09-04 22:43:35 -0300

    Aprendemos a criar o banco de dados do projeto

--- obj/Debug/net8.0/RpgApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/RpgApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RpgApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ddc1b85ae93da3e5a8fbca3ec2cc5b90c92bc1be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6ab98e0a0dd5fa123a694c648c2b2e3106103db")]
 [assembly: System.Reflection.AssemblyProductAttribute("RpgApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RpgApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
